7c68f7cffb4da1381031c8b11bfcbaa92f04e872
PorcoRosso85 <40359643+PorcoRosso85@users.noreply.github.com>
2022-11-06 20:06:55 +0900

    Update dots.sh

--- .install/dots.sh
+++ .install/dots.sh
@@ -1,6 +1,6 @@
 git clone --bare git@github.com:PorcoRosso85/dots.git $HOME/.dotfiles
 alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-rm -r .dotfiles/ .config/ .install/ .bashrcs/ .gitconfig
+rm -r .dotfiles/ .config/ .install/ .bashrcs/ .gitconfig .README.md
 echo 'if you already have dotfiles, remove before clone'
 
 git clone --bare git@github.com:PorcoRosso85/dots.git $HOME/.dotfiles
