0312c00e345dfb21161d5f0643f14f6e63ff93cc
PorcoRosso85 <40359643+PorcoRosso85@users.noreply.github.com>
2022-11-06 19:42:25 +0900

    Delete .dots.sh

--- .dots.sh
+++
@@ -1,4 +0,0 @@
-alias dots='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-rm -r .dotfiles/ .config/ .install/ .dots.sh .bashrc .bashrcs/ .gitconfig
-echo 'if you already have dotfiles, remove before clone'
-git clone --bare git@github.com:PorcoRosso85/dots.git $HOME/.dotfiles
