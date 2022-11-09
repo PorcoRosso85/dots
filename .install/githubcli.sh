70fb342a67f24b6b779038071d2a1d91a6385915
root <root@DESKTOP-1PF4AT3.localdomain>
2022-11-09 14:28:09 +0900

    git/github cli, cli allows us to use cli only for ssh connection

--- .install/githubcli.sh
+++ .install/githubcli.sh
@@ -1,3 +1,10 @@
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/about-ssh
+# ls ~/.ssh
+# ssh-keygen -t ed25519 -C "<this os/machine unique auth note>"
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_ed25519
+# cat ~/.ssh/id_ed25519.pub
+#
 type -p curl >/dev/null || apt install curl -y
 
 # curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
@@ -7,3 +14,13 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | apt-k
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 && apt update \
 && apt install gh -y
+
+# gh auth login     # choose ssh login, and verify it with browser because of easy
+# ssh -T git@github.com
+# nvim ~/.ssh/config
+# Host github github.com
+    #HostName github.com
+    #User git
+    #IdentityFile ~/.ssh/github
+# https://gist.github.com/vikpe/34454d69fe03a9617f2b009cc3ba200b
+# ssh-keyscan github.com >> ~/.ssh/known_hosts
--- .install/nvm_npm.sh
+++ .install/nvm_npm.sh
@@ -1,3 +1,7 @@
 # https://github.com/nvm-sh/nvm#installing-and-updating
 
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
+echo 'if you've done, printed 'nvm ( not work which command )'
+command -v nvm
+# issue https://github.com/nvm-sh/nvm/issues/2918
